f7380185eeb3612f5e7283306d775694c5f14a49
R.soorya <sooryayaron147@gmail.com>
2023-07-05 21:30:41 +0530

    Fixing bug

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import  index from "./index.css"
 import { Provider } from 'react-redux';
 import store from './store';
 
