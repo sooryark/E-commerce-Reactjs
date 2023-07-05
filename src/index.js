b6958c6b8016a27ff087f66717501ae0c50935a5
R.soorya <sooryayaron147@gmail.com>
2023-07-05 21:39:00 +0530

    changeing File Name

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './store';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
